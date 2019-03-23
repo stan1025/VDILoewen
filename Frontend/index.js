a5c2383a23fce0210fa2f9e1673eebc04b4dbc29
benedikt255 <benedikt.geib@googlemail.com>
2019-03-23 09:44:20 +0100

    Fronteend: +comments 1st level

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,24 +1,26 @@
 var app = angular.module('VDILionExample', []);
 const server = "http://localhost:3000";
-//make global because bindings cause trouble; check
-//var userID = "";
 
 app.controller('ProfileController', function ($scope, $http) {
     ctrl = this;
+    //Initialwerte schreiben
     $scope.title = 'Benutzerprofil';
     $scope.screen = 'Login';
     $scope.userID ='';
 
+    //Callback-Funktion für die LogIn Seite, setzt den aktiven Benutzer und schaltet die Seite um
     $scope.login = function(user) {
         $scope.userID = user;
         $scope.screen = 'Userprofile';
     }
 
+    //Callback-Funktion für die Member Seite, setzt den aktiven Benutzer zurück und schaltet die Seite um
     $scope.logout = function () {
         $scope.userID = '';
         $scope.screen ='Logout';
     }
 
+    //Callback-Funktion für die LogOut Seite, setzt den aktiven Benutzer zurück und schaltet die Seite um
     $scope.home = function () {
         $scope.userID = '';
         $scope.screen ='Login';
---
+++ Frontend/readme.txt
@@ -0,0 +1,15 @@
+VDI Digital Prototyp Beipsiel
+
+Die Einstiegsseite ist Index.html. Dazu gehört die Javascript-Datei Index.js.
+Dort finden sich links zu weiterführender Dokumentation und Kommentare im Code. Die Kommentare im Code werden weinger detailiert, je weiter man in der Hierachieebene nach unten wandert.
+
+Hierachie:
+Index
+-LogIn
+-profileView
+--editableField
+--SkillList
+---skillDetail
+----editableField
+----starRating
+-LogOut
